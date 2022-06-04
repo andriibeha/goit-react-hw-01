1c29adb2c0a3704349e62b186fa8ec4e71663bd1
andriibeha <beha.andrii@gmail.com>
2022-06-04 16:32:41 +0300

    Component Portfolio

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import user from "../user/user.json"
+import Profile from "./Profile/Profile";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      Hello World!
-    </div>
+    <Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+     />
   );
 };
